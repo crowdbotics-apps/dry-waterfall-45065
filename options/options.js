788b1a834321a5617256147efc97f50c65bffa0f
Crowdbotics <team@crowdbotics.com>
2023-12-17 20:17:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-waterfall-45065",
+  url: "https://dry-waterfall-45065.botics.co",
+  api: "https://dry-waterfall-45065.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
